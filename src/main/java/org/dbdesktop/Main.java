d2b7eafd86215173743e9e9d3256e8b553e15f71
Nick Mukhin <mukhin.nick@gmail.com>
2025-05-28 13:17:29 +0300

    Initial commit

---
+++ src/main/java/org/dbdesktop/Main.java
@@ -0,0 +1,54 @@
+package org.dbdesktop;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(@org.jetbrains.annotations.NotNull String[] args) {
+        String host = "localhost";
+        String port = "3306";
+        String database;
+        String username;
+        String password;
+        switch (args.length) {
+            case 3:
+                database = args[0];
+                username = args[1];
+                password = args[2];
+                break;
+            case 4:
+                try {
+                    int portNum = Integer.parseInt(args[0]);
+                    port = "" + portNum;
+                } catch (NumberFormatException ne) {
+                    host = args[0];
+                }
+                database = args[1];
+                username = args[2];
+                password = args[3];
+                break;
+            case 5:
+                host = args[0];
+                port = args[1];
+                database = args[2];
+                username = args[3];
+                password = args[4];
+                break;
+            default:
+                System.out.println("Usage: java org.dbdesktop.Main <host> <port> <database> <username> <password>");
+                System.out.println("   Or: java org.dbdesktop.Main [<host> | <port>] <database> <username> <password>");
+                System.out.println("   Or: java org.dbdesktop.Main <database> <username> <password>");
+                return;
+        }
+
+
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=false&serverTimezone=UTC";
+
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            System.out.println("✅ Successfully connected to the database.");
+        } catch (SQLException e) {
+            System.err.println("❌ Connection failed: " + e.getMessage());
+        }
+    }
+}
